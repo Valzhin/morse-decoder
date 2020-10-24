0733306961c205620dabe735a84ecb1b7536e122
Valzhin <70908623+Valzhin@users.noreply.github.com>
2020-10-24 17:14:52 +0300

    init: start morse-decoder-task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = [];
+    let clean = '';
+    let finalArr = '';
+    for (let i = 0; i <= expr.length; i++) {
+      if (i % 10 === 0 && i !== 0) {
+        if (expr.substring(i - 10, i) === '**********') {
+          clean = clean + '**'
+        } else {
+          clean = clean + ' ' + (expr.substring(i - 10, i) * 1) + ' '
+        }
+      }
+    }
+    clean = clean.split(' ')
+    clean.forEach(r => {
+      let count = ''
+      for (let i = 0; i <= r.length; i++) {
+        if (i % 2 === 0 && i !== 0) {
+          if (r.substring(i - 2, i) === '10') {
+            count = count + '.'
+          } else if (r.substring(i - 2, i) === '11') {
+            count = count + '-'
+          } else if (r.substring(i - 2, i) === '**') {
+            count = count + '*'
+          }
+        }
+      }
+      arr.push(count)
+    })
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] && arr[i] !== '*') {
+        finalArr = finalArr + MORSE_TABLE[arr[i]]
+      } else if (arr[i] === '*') {
+        finalArr = finalArr + ' '
+      }
+    }
+    return finalArr
+  }
 
 module.exports = {
     decode
